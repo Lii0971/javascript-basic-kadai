6b1d522d03fc859429a0fda40cb7b6ec6f439298
Lii0971 <kurumi.tamada@gmail.com>
2025-02-05 13:21:07 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,13 @@
-// 定数の宣言・値の代入
-const weight = 68;
-const height = 1.7;
 
-// 四則演算
-console.log(weight / (height * height));
+let weight; // 体重 (kg)
+let height; // 身長 (m)
+let bmi;    // BMIを保存するための変数
+
+weight = 68;
+height = 1.7;
+
+// BMIを計算し、変数に代入します
+bmi = weight / (height * height);
+
+// BMIの結果を表示します
+console.log(bmi);
