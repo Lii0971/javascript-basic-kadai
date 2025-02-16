1526cfa371e0eac8bd95aaddb4fbe55545c18ab2
Lii0971 <kurumi.tamada@gmail.com>
2025-02-16 18:45:06 +0900

    Add files via upload

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,6 +1,13 @@
-const btn = document.getElementById('bin');
+//btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+//textというidを持つHTML要素を取得し、定数に代入する
 const text = document.getElementById('text');
 
-btn.addEventListener('clik', () => {
-  text.innerText = 'ボタンをクリックしました';
+//btnをクリックした時のイベント処理を実行する
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+     //textItemの文字を2秒後に置き換える
+      text.textContent = 'ボタンをクリックしました';
+  }, 2000);
 });
