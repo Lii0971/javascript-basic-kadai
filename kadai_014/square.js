ebadc6615c5c1f090753ca2af71872c6e7556b9d
Lii0971 <kurumi.tamada@gmail.com>
2025-02-16 11:41:34 +0900

    Add files via upload

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,8 +1,6 @@
-const calculateTotal = (price) => {
-  // 与えられた引数priceを2乗
-  console.log(price ** 2);
+
+function square(num) {
+  return num * num;
 }
 
-
-// 関数を呼び出し、引数として購入金額を渡す
-calculateTotal(10);
+console.log(square(10));
